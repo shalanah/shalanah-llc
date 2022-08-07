f1379d32de1e961b7b3442b928bd672da01bd1af
shalanah <shalanahfaith@gmail.com>
2022-08-06 21:03:31 -0500

    Starting to outline site

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,27 +3,42 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Shalanah LLC`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.shalanah.llc`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`vampiro one`],
+        display: "swap",
+      },
+    },
+  ],
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,136 @@
-import * as React from "react"
-import type { HeadFC } from "gatsby"
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+import styled, { createGlobalStyle } from "styled-components";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+// Using Gatsby - although it is overkill for now - to allow easy routing and possible blog in the future
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+const GlobalStyles = createGlobalStyle`
+  body, html, #___gatsby {
+    height: 100%;
+    width: 100%;
   }
-]
+  *, *:before, *:after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-size: 1.2rem;
+    background: #222;
+    color: #fff;
+    font-family: sans-serif;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: vampiro one, sans-serif;
+  }
+  h2 {
+    font-size: 2.5rem;
+  }
+`;
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+// TODO: Make a layout style?
+// TODO: SEO info
 
-const links = [
+const AppArticle = styled.article`
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: min(700px, 50vw) auto;
+  margin-bottom: 1rem;
+  gap: 1rem;
+  h2 {
+    margin-bottom: 0.5rem;
+  }
+  .img {
+    aspect-ratio: 2;
+    width: 100%;
+    border-radius: 30px;
+    background: blue;
+  }
+  .text {
+    display: flex;
+    flex-direction: column;
+    > div {
+      width: 100%;
+      margin: auto;
+    }
+  }
+  grid-template-areas: "img   text";
+`;
+const Section = styled.section``;
+const Footer = styled.footer``;
+const Header = styled.header``;
+
+const apps = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    name: "Spiral Betty",
+    launched: "2018",
+    description: "Spiralize your photos",
+    url: "https://spiralbetty.com",
+    playstore: "",
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    name: "Kind Cloud",
+    launched: "2020",
+    description: "A sound-free loving-kindness meditation app",
+    url: "https://kindcloud.app",
+    playstore: "",
   },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+];
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+    <>
+      <GlobalStyles />
+      <main
+        style={{
+          padding: "20px",
+          maxWidth: "1400px",
+          margin: "max(2rem, 4vh) auto",
+        }}
+      >
+        <Header>
+          <h1 style={{ fontSize: "2vw" }}>
+            Shalanah<span style={{ fontSize: ".6em" }}> LLC</span>
+          </h1>
+        </Header>
+        {apps.map(({ name, description, launched, url }) => {
+          return (
+            <AppArticle>
+              <div className="img" style={{ gridArea: "img" }} />
+              <div className="text" style={{ gridArea: "text" }}>
+                <div>
+                  <h2 translate="no">{name}</h2>
+                  <p className="launched">Launched {launched}</p>
+                  <p className="description">{description}</p>
+                  <p className="link">
+                    <a href={url}>Link</a>
+                  </p>
+                </div>
+              </div>
+            </AppArticle>
+          );
+        })}
+        <Section>
+          <h2>About</h2>
+          <p>
+            Shalanah Dawson created Shalanah LLC in 2021 after{" "}
+            <span translate="no">Spiral Betty</span> became a viral hit among
+            coloring book enthusiasts, Cricut/Silhoutte cutting machine users,
+            and as mobile iPhone lockscreens.
+          </p>
+          <p>
+            Shalanah continues to work on{" "}
+            <span translate="no">Spiral Betty</span>,{" "}
+            <span translate="no">Kind Cloud</span>, and other web app projects.
+          </p>
+          <p>For inquiries, please email ...</p>
+        </Section>
+        <Footer>Shalanah LLC 2022</Footer>
+      </main>
+    </>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Shalanah LLC</title>;
